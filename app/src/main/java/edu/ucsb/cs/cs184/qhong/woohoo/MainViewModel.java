8a5f99df601330b153a84bcc49482c6eb2e8e351
Icarus99 <jiajun_li@ucsb.edu>
2020-12-12 18:44:47 +0800

    added loading sign and enabled add friends

--- app/src/main/java/edu/ucsb/cs/cs184/qhong/woohoo/MainViewModel.java
+++ app/src/main/java/edu/ucsb/cs/cs184/qhong/woohoo/MainViewModel.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs.cs184.qhong.woohoo;
 
 import android.renderscript.Sampler;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
@@ -15,23 +16,128 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import edu.ucsb.cs.cs184.qhong.woohoo.utils.FriendGroup;
+import edu.ucsb.cs.cs184.qhong.woohoo.utils.User;
+
 public class MainViewModel extends ViewModel {
 
     private MutableLiveData<String> mEmail;
-    private FirebaseAuth mAuth;
     private MutableLiveData<String> mNickname;
+    private MutableLiveData<ArrayList<FriendGroup>> mFriends;
+    private FirebaseAuth mAuth;
+
+    public boolean fetched = true;
+
+    private MutableLiveData<User> user;
 
     public MainViewModel() {
         mEmail = new MutableLiveData<>();
         mEmail.setValue("");
         mNickname = new MutableLiveData<>();
         mNickname.setValue("");
-
+        user = new MutableLiveData<>();
         mAuth = FirebaseAuth.getInstance();
+        mFriends = new MutableLiveData<>();
+        initFriendList();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser!=null){
+            updateFriendsList(currentUser);
             mEmail.setValue(currentUser.getEmail());
+            //fetchAllUsers();
         }
+
+
+    }
+    public void fetchCurUsers(final String UID){
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Users");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot shot : snapshot.getChildren()) {
+                    if (UID.equals(shot.getKey())){
+                        User tempUser = new User();
+                        tempUser.setUid(shot.getKey());
+                        tempUser.setName(shot.child("name").getValue(String.class));
+                        tempUser.setEmail(shot.child("email").getValue(String.class));
+                        tempUser.setIcon("Default profile photo");
+                        ArrayList<FriendGroup> tempGroups = new ArrayList<>();
+                        for (DataSnapshot s : shot.child("friend").getChildren()) {
+                            FriendGroup temp = new FriendGroup();
+                            temp.setGroupName(s.getKey());
+                            ArrayList<User> tempList = new ArrayList<>();
+                            for (DataSnapshot sUser : s.getChildren()) {
+                                User t = new User();
+                                t.setUid(sUser.getKey());
+                                t.setName(sUser.child("name").getValue(String.class));
+                                t.setEmail(sUser.child("email").getValue(String.class));
+                                t.setIcon("Default profile photo");
+                                tempList.add(t);
+                            }
+                            temp.setFriends(tempList);
+                            tempGroups.add(temp);
+
+                        }
+                        tempUser.setFriends(tempGroups);
+                        user.setValue(tempUser);
+                    }
+                }
+                fetched = false;
+                Log.e("Tag","Fetch success");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void initFriendList(){
+        ArrayList<FriendGroup> groups = new ArrayList<>();
+        FriendGroup temp = new FriendGroup();
+        temp.setGroupName("Default Group");
+        ArrayList<User> list = new ArrayList<>();
+        temp.setFriends(list);
+        groups.add(temp);
+        mFriends.setValue(groups);
+    }
+
+    public void updateFriendsList(FirebaseUser currentUser){
+        FirebaseDatabase.getInstance().getReference("Users").child(currentUser.getUid()).child("friend")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        ArrayList<FriendGroup> groups = new ArrayList<>();
+                        for (DataSnapshot shot : snapshot.getChildren()) {
+                            FriendGroup temp = new FriendGroup();
+                            temp.setGroupName(shot.getKey());
+                            ArrayList<User> list = new ArrayList<>();
+                            for(DataSnapshot s : shot.getChildren()){
+                                User u = new User();
+                                u.setName(s.child("name").getValue(String.class));
+                                u.setEmail(s.child("email").getValue(String.class));
+                                list.add(u);
+                            }
+                            temp.setFriends(list);
+                            groups.add(temp);
+                        }
+                        if(groups.size()!=0){
+                            mFriends.setValue(groups);
+                        }
+                        fetched = false;
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    public MutableLiveData<ArrayList<FriendGroup>> getmFriends() {
+        return mFriends;
     }
 
     public MutableLiveData<String> getmNickname() {
--- app/src/main/java/edu/ucsb/cs/cs184/qhong/woohoo/ui/friend/FriendFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs184/qhong/woohoo/ui/friend/FriendFragment.java
@@ -16,8 +16,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +30,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 import edu.ucsb.cs.cs184.qhong.woohoo.LoginActivity;
+import edu.ucsb.cs.cs184.qhong.woohoo.MainViewModel;
 import edu.ucsb.cs.cs184.qhong.woohoo.R;
 import edu.ucsb.cs.cs184.qhong.woohoo.utils.FriendGroup;
 import edu.ucsb.cs.cs184.qhong.woohoo.utils.MyAdapter;
@@ -34,27 +38,32 @@ import edu.ucsb.cs.cs184.qhong.woohoo.utils.User;
 
 public class FriendFragment extends Fragment {
 
-    private FriendViewModel friendViewModel;
+    private MainViewModel mViewModel;
 
-    private String[] groupNames = new String[]{"女朋友", "宠物", "基友", "小弟"};
-    private String[][] friendNames = new String[][]{{"苍井空", "波多野结衣", "小泽玛莉亚", "龙泽罗拉"},
-            {"草泥马", "雅蠛蝶", "法克鱿"},
-            {"小张", "小杨", "小洪", "小李", },
-            {"奥巴驴", "小学僧"}};
-    private ArrayList<FriendGroup> groups = new ArrayList<>();
+    private ArrayList<FriendGroup> groups;
     private MyAdapter mAdapter;
     private ExpandableListView expandableListView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        friendViewModel =
-                ViewModelProviders.of(this).get(FriendViewModel.class);
+        mViewModel =
+                ViewModelProviders.of(this).get(MainViewModel.class);
+
         final View root = inflater.inflate(R.layout.fragment_friend, container, false);
         expandableListView = root.findViewById(R.id.expandableView);
-        initList();
+//        initList();
+        groups = mViewModel.getmFriends().getValue();
         initListener();
         mAdapter = new MyAdapter(groups,getContext());
         expandableListView.setAdapter(mAdapter);
+        mViewModel.getmFriends().observe(getViewLifecycleOwner(), new Observer<ArrayList<FriendGroup>>() {
+            @Override
+            public void onChanged(ArrayList<FriendGroup> friendGroups) {
+                groups = friendGroups;
+                mAdapter = new MyAdapter(groups,getContext());
+                expandableListView.setAdapter(mAdapter);
+            }
+        });
 
         final Button addButton = root.findViewById(R.id.searchUser);
         addButton.setOnClickListener(new View.OnClickListener(){
@@ -77,21 +86,52 @@ public class FriendFragment extends Fragment {
                         return false;
                     }
                 });
+                expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+                    @Override
+                    public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+                        return false;
+                    }
+                });
                 userSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                     @Override
                     public boolean onQueryTextSubmit(final String s) {
 
-                        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Users");
+                        final DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Users");
                         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 boolean t = true;
-                                for (DataSnapshot sshot : snapshot.getChildren()) {
+                                for (final DataSnapshot sshot : snapshot.getChildren()) {
                                     String temp = sshot.child("name").getValue(String.class);
                                     if (temp.equals(s)) {
                                         LinearLayout findUser = root.findViewById(R.id.findUser);
                                         TextView findUserName = root.findViewById(R.id.findUserName);
                                         TextView findUserEmail = root.findViewById(R.id.findUserEmail);
+                                        Button findUserAdd = root.findViewById(R.id.addFindUser);
+                                        findUserAdd.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                User u = new User();
+                                                u.setName(s);
+                                                u.setEmail(sshot.child("email").getValue().toString());
+                                                if(groups.size()==0){
+                                                    FriendGroup temp = new FriendGroup();
+                                                    temp.setGroupName("Default Group");
+                                                    ArrayList<User> list = new ArrayList<>();
+                                                    temp.setFriends(list);
+                                                    groups.add(temp);
+                                                }
+                                                groups.get(0).getFriends().add(u);
+                                                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                                                FirebaseUser currentUser = mAuth.getCurrentUser();
+                                                DatabaseReference curUser = myRef.child(currentUser.getUid()).child("friend")
+                                                        .child("Default Group").child(sshot.getKey());
+                                                curUser.child("name").setValue(s);
+                                                curUser.child("email").setValue(sshot.child("email").getValue());
+                                                curUser.child("icon").setValue("Default profile photo");
+
+                                            }
+                                        });
                                         findUserName.setText(s);
                                         findUserEmail.setText(sshot.child("email").getValue(String.class));
                                         findUser.setVisibility(View.VISIBLE);
@@ -126,20 +166,20 @@ public class FriendFragment extends Fragment {
             }});
         return root;
     }
-    public void initList(){
-        for(int i = 0;i < groupNames.length;i++){
-            FriendGroup temp = new FriendGroup();
-            temp.setGroupName(groupNames[i]);
-            ArrayList<User> list = new ArrayList<>();
-            for(int j = 0;j < friendNames[i].length;j++){
-                User u = new User();
-                u.setName(friendNames[i][j]);
-                list.add(u);
-            }
-            temp.setFriends(list);
-            groups.add(temp);
-        }
-    }
+//    public void initList(){
+//        for(int i = 0;i < groupNames.length;i++){
+//            FriendGroup temp = new FriendGroup();
+//            temp.setGroupName(groupNames[i]);
+//            ArrayList<User> list = new ArrayList<>();
+//            for(int j = 0;j < friendNames[i].length;j++){
+//                User u = new User();
+//                u.setName(friendNames[i][j]);
+//                list.add(u);
+//            }
+//            temp.setFriends(list);
+//            groups.add(temp);
+//        }
+//    }
     public void initListener(){
         expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
