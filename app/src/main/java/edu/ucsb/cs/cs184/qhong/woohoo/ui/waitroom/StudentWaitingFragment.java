7f027ceb0d4a9405cebfb2e92f5cf96457c748b9
qinghanghong1 <qhong@umail.ucsb.edu>
2020-12-12 21:01:59 +0800

    change scoreboard, add live number of players in waiting room

--- app/src/main/java/edu/ucsb/cs/cs184/qhong/woohoo/ui/waitroom/StudentWaitingFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs184/qhong/woohoo/ui/waitroom/StudentWaitingFragment.java
@@ -22,12 +22,18 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 import edu.ucsb.cs.cs184.qhong.woohoo.FindViewModel;
 import edu.ucsb.cs.cs184.qhong.woohoo.QuizActivity;
 import edu.ucsb.cs.cs184.qhong.woohoo.R;
 import edu.ucsb.cs.cs184.qhong.woohoo.ui.setting.SettingFragment;
+import edu.ucsb.cs.cs184.qhong.woohoo.utils.Player;
 
 //YZ2nd: wait room for students
 public class StudentWaitingFragment extends Fragment {
@@ -75,15 +81,21 @@ public class StudentWaitingFragment extends Fragment {
                                 ((Long)snapshot.child("TimePerQuestion").getValue()).intValue();
                         data.putInt("time_per_ques", timePerQuestion);
                     }
-
+                    if(snapshot.hasChild("players")){
+                        GenericTypeIndicator<ArrayList<Player>> t = new GenericTypeIndicator<ArrayList<Player>>() {};
+                        ArrayList<Player> players = (ArrayList<Player>)snapshot.child("players").getValue(t);
+                        TextView numPlayerText = (TextView) getActivity().findViewById(R.id.numberOfPlayersText);
+                        numPlayerText.setText(players.size() + " players in the room");
+                    }
                     if(snapshot.hasChild("start")){
                         data.putInt("room_id", mViewModel.getCode().getValue());
                         intent.putExtras(data);
                         startActivity(intent);
-                    }else{
-                        Toast.makeText(getContext(), "Quiz is going to start!",
-                                Toast.LENGTH_LONG).show();
                     }
+//                    else{
+//                        Toast.makeText(getContext(), "Quiz is going to start!",
+//                                Toast.LENGTH_LONG).show();
+//                    }
                 }
 
             @Override
@@ -91,6 +103,7 @@ public class StudentWaitingFragment extends Fragment {
 
             }
         });
+
     }
 
 }
